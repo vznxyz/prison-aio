88a930284f92e88274479162b84dc80d41f512e5
Joel Evans <joeleoli99@gmail.com>
2020-07-29 06:29:46 -0400

    Fix missing slash in leaderboards clickable fancy message that executes cmd

--- src/main/kotlin/net/evilblock/prisonaio/module/leaderboard/command/IndexCommand.kt
+++ src/main/kotlin/net/evilblock/prisonaio/module/leaderboard/command/IndexCommand.kt
@@ -29,13 +29,13 @@ object IndexCommand {
             val message = FancyMessage("${ChatColor.GRAY}${Constants.DOUBLE_ARROW_RIGHT} ${ChatColor.RED}${ChatColor.BOLD}${leaderboard.name} ")
                 .then("${ChatColor.GRAY}[${ChatColor.GREEN}${ChatColor.BOLD}VIEW${ChatColor.GRAY}]")
                 .formattedTooltip(FancyMessage("${ChatColor.YELLOW}Click to view this leaderboard."))
-                .command("lb results ${leaderboard.id}")
+                .command("/lb results ${leaderboard.id}")
 
             if (sender.hasPermission("")) {
                 message.then(" ")
                     .then("${ChatColor.GRAY}[${ChatColor.AQUA}${ChatColor.BOLD}SPAWN${ChatColor.GRAY}]")
                     .formattedTooltip(FancyMessage("${ChatColor.YELLOW}Click to spawn this leaderboard NPC."))
-                    .command("lb spawn ${leaderboard.id}")
+                    .command("/lb spawn ${leaderboard.id}")
             }
 
             message.send(sender)
